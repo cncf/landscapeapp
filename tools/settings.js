ae04f3141b2ab8c3a1e5cebc7f57df99af6dbf1e
ZeusTheTrueGod <l-2732@yandex.ru>
2018-12-10 06:47:23 +0000

    require PROJECT_PATH

--- tools/settings.js
+++ tools/settings.js
@@ -1,3 +1,8 @@
 import path from 'path';
 export const projectPath = process.env.PROJECT_PATH;
+if (!projectPath) {
+  console.info('FATAL: the PROJECT_PATH env variable is not set. Please point it to the cncf, lfdl or other landscape repo');
+  console.info(process.env);
+  process.exit(1);
+}
 export const settings = require('js-yaml').safeLoad(require('fs').readFileSync(path.resolve(projectPath, 'settings.yml')));
