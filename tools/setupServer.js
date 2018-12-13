01a2c22ea714ecaa9eaee180e397370c3dfdbc91
ZeusTheTrueGod <l-2732@yandex.ru>
2018-12-13 19:41:06 +0000

    ensure that we have a proper update server script

---
+++ tools/setupServer.js
@@ -0,0 +1,27 @@
+import { settings } from './settings';
+import ejs from 'ejs';
+const file = require('fs').readFileSync('update_server/setup.template', 'utf-8');
+
+const values = {
+  repo: settings.global.repo,
+  email: settings.global.email,
+  ip: settings.update_server.ip,
+  update_hour: settings.update_server.update_hour,
+  update_minute: settings.update_server.update_minute
+}
+const content = ejs.render(file, values);
+require('fs').writeFileSync('/tmp/update_server.bash', content);
+var spawn = require('child_process').spawn;
+var child = spawn('bash', ['/tmp/update_server.bash']);
+child.stdout.on('data', function(data) {
+    console.log(data.toString('utf-8'));
+    //Here is where the output goes
+});
+child.stderr.on('data', function(data) {
+    console.log(data.toString('utf-8'));
+    //Here is where the error output goes
+});
+child.on('close', function(code) {
+    console.log('done');
+    //Here you can get the exit code of the script
+});
