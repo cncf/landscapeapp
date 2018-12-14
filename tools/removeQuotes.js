346028ecd907fc6d7f6d045820c275fe0a4ef77c
ZeusTheTrueGod <l-2732@yandex.ru>
2018-12-14 15:35:19 +0000

    update removeQuotes

--- tools/removeQuotes.js
+++ tools/removeQuotes.js
@@ -1,12 +1,18 @@
 import _ from 'lodash';
 import path from 'path';
 import { projectPath } from './settings';
-const source = require('js-yaml').safeLoad(require('fs').readFileSync(path.resolve(projectPath, 'landscape.yml')));
-_.each(source.landscape, function(category) {
-  _.each(category.subcategories, function(subcategory) {
-    subcategory.items = _.orderBy(subcategory.items, (x) => x.name.toUpperCase());
+
+const cleanupFile = function(filename) {
+  const source = require('js-yaml').safeLoad(require('fs').readFileSync(path.resolve(projectPath, filename)));
+  _.each(source.landscape, function(category) {
+    _.each(category.subcategories, function(subcategory) {
+      subcategory.items = _.orderBy(subcategory.items, (x) => x.name.toUpperCase());
+    });
   });
-});
-var dump = require('js-yaml').dump(source, {lineWidth: 160});
-dump = dump.replace(/(- \w+:) null/g, '$1');
-require('fs').writeFileSync(path.resolve('landscape.yml', projectPath), dump);
+  var dump = require('js-yaml').dump(source, {lineWidth: 160});
+  dump = dump.replace(/(- \w+:) null/g, '$1');
+  require('fs').writeFileSync(path.resolve(projectPath, filename), dump);
+}
+
+cleanupFile('landscape.yml');
+cleanupFile('settings.yml');
