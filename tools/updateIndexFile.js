cb04fcb5af84b4401a090198d3077dae42a7fea6
ZeusTheTrueGod <l-2732@yandex.ru>
2018-12-16 18:50:28 +0000

    update index.html so navigation should start to work properly

--- tools/updateIndexFile.js
+++ tools/updateIndexFile.js
@@ -17,11 +17,12 @@ function update(original, updated, file) {
 }
 
 
-update('/main', `/${projectName}/main`, 'index.html'); // - in index.html
-update('/favicon', `/${projectName}/favicon`, 'index.html'); // - in index.html
-update('/assets', `/${projectName}/assets`, 'index.html'); // - in index.html
-update('url(/', `url(/${projectName}/`, 'main.*.css'); // - in main.css
-update('/images', `/${projectName}/images`, 'main.*.js'); // - in  main.js
-update('/logos', `/${projectName}/logos`, 'main.*.js'); // - in main.js
-update('/data.json', `/${projectName}/data.json`, 'main.*.js') // - in main.js
-update('/logos', `/${projectName}/logos`, 'data.json') // - in data.json
+update('/main', `/${projectName}/main`, 'index.html');
+update('/favicon', `/${projectName}/favicon`, 'index.html');
+update('/assets', `/${projectName}/assets`, 'index.html');
+update('url(/', `url(/${projectName}/`, 'main.*.css');
+update('/images', `/${projectName}/images`, 'main.*.js');
+update('/logos', `/${projectName}/logos`, 'main.*.js');
+update('/data.json', `/${projectName}/data.json`, 'main.*.js');
+update('/logos', `/${projectName}/logos`, 'data.json');
+update('window.prefix=""', `window.prefix="/${projectName}"`, 'index.html');
