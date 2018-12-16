4982f6465faa268941614d0d9656c307cf669740
ZeusTheTrueGod <l-2732@yandex.ru>
2018-12-16 20:05:18 +0000

    remove index.html reference

--- build.sh
+++ build.sh
@@ -10,7 +10,7 @@ mkdir -p dist
 cp -r $2/dist dist/$2
 babel-node tools/updateIndexFile.js $2
 echo "/$2/* /$2/index.html 200" >> dist/_redirects
-echo "<div><a href="$2/index.html"><h1>$2</h1></a></div>" >> dist/index.html
+echo "<div><a href="$2/"><h1>$2</h1></a></div>" >> dist/index.html
 
 
 
