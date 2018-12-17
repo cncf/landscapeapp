5321c9432322c3c551ac7509ecc75b382c8aef46
ZeusTheTrueGod <l-2732@yandex.ru>
2018-12-17 20:28:55 +0000

    and one more fix

--- src/reducers/api.js
+++ src/reducers/api.js
@@ -1,7 +1,7 @@
 import preview from 'project/preview.json';
 import unpack from '../utils/unpackArray';
 export async function loadData() {
-  const data = await (await fetch('/data.json')).json();
+  const data = await (await fetch('data.json')).json();
   return data;
 }
 
