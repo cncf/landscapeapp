19848608be09b7659d434872020bf8b547e2678a
ZeusTheTrueGod <l-2732@yandex.ru>
2018-12-18 07:41:50 +0000

    testing commits to the master repo

--- full_build.sh
+++ full_build.sh
@@ -1,5 +1,7 @@
 set -e
 rm -rf dist || true
 mkdir -p dist
-bash build.sh cncf/landscape cncf 1015-try-upstream
-bash build.sh LFDLFoundation/landscape lfdl 33-switch-to-upstream
+# bash build.sh cncf/landscape cncf 1015-try-upstream
+# bash build.sh LFDLFoundation/landscape lfdl 33-switch-to-upstream
+git commit -m 'lets test! [skip ci]' --allow-empty
+git push origin HEAD
