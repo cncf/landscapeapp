b68c931e2433252a9772a89537f6d31956fbc01e
ZeusTheTrueGod <l-2732@yandex.ru>
2018-12-18 12:26:03 +0000

    one more attempt

--- full_build.sh
+++ full_build.sh
@@ -4,7 +4,7 @@ mkdir -p dist
 # bash build.sh cncf/landscape cncf 1015-try-upstream
 # bash build.sh LFDLFoundation/landscape lfdl 33-switch-to-upstream
 git remote rm github || true
-git remote add github 'https://$GITHUB_USER:$GITHUB_TOKEN@github.com/cncf/landscapeapp'
+git remote add github "https://$GITHUB_USER:$GITHUB_TOKEN@github.com/cncf/landscapeapp"
 git fetch github
 git config --global user.email "info@cncf.io"
 git config --global user.name "Netlify Publisher"
