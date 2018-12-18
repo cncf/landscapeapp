672665f138d226f7e229175ae9f928b29ce2d9a1
ZeusTheTrueGod <l-2732@yandex.ru>
2018-12-18 12:18:50 +0000

    one more test

--- full_build.sh
+++ full_build.sh
@@ -3,7 +3,10 @@ rm -rf dist || true
 mkdir -p dist
 # bash build.sh cncf/landscape cncf 1015-try-upstream
 # bash build.sh LFDLFoundation/landscape lfdl 33-switch-to-upstream
+git remote rm github || true
+git remote add github https://$GITHUB_USER:$GITHUB_TOKEN@github.com/cncf/landscapeapp
+git fetch github
 git config --global user.email "info@cncf.io"
 git config --global user.name "Netlify Publisher"
 git commit -m 'lets test! [skip ci]' --allow-empty
-git push origin HEAD --tags
+git push github HEAD --tags
