b4cf7639e641bdd510e87fd6924fa630eceb5023
ZeusTheTrueGod <l-2732@yandex.ru>
2018-12-18 15:05:14 +0000

    returning to remotes

--- full_build.sh
+++ full_build.sh
@@ -3,12 +3,10 @@ rm -rf dist || true
 mkdir -p dist
 # bash build.sh cncf/landscape cncf 1015-try-upstream
 # bash build.sh LFDLFoundation/landscape lfdl 33-switch-to-upstream
-cd /tmp
-git clone "https://$GITHUB_USER:$GITHUB_TOKEN@github.com/cncf/landscapeapp"
-cd landscapeapp
-git branch
+git remote rm github || true
+git remote add github "https://$GITHUB_USER:$GITHUB_TOKEN@github.com/cncf/landscapeapp"
+git fetch github
 git config --global user.email "info@cncf.io"
 git config --global user.name "Netlify Publisher"
 git commit -m 'lets test! [skip ci]' --allow-empty
-git push origin HEAD
-git push origin HEAD --tags
+git push github HEAD --tags
