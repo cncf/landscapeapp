14c71844a2298e53fdb4cb0bedf356f603e82ede
ZeusTheTrueGod <l-2732@yandex.ru>
2018-12-18 21:09:13 +0000

    and one more fix

--- full_build.sh
+++ full_build.sh
@@ -1,8 +1,6 @@
 set -e
 rm -rf dist || true
 mkdir -p dist
-git config --global user.email "info@cncf.io"
-git config --global user.name "Netlify Publisher"
 
 bash build.sh LFDLFoundation/landscape lfdl 33-switch-to-upstream
 bash build.sh cncf/landscape cncf 1015-try-upstream
@@ -10,6 +8,8 @@ bash build.sh cncf/landscape cncf 1015-try-upstream
 # This will increase a version and publish to an npm
 # If there is an existing package
 if [ $BRANCH = "master" ]; then
+  git config --global user.email "info@cncf.io"
+  git config --global user.name "Netlify Publisher"
   git remote rm github || true
   git remote add github "https://$GITHUB_USER:$GITHUB_TOKEN@github.com/cncf/landscapeapp"
   git fetch github
