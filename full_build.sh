ee0a7b8da989709622f504cd4d7294ab96f16c0f
ZeusTheTrueGod <l-2732@yandex.ru>
2018-12-18 12:14:23 +0000

    a new version

--- full_build.sh
+++ full_build.sh
@@ -3,5 +3,7 @@ rm -rf dist || true
 mkdir -p dist
 # bash build.sh cncf/landscape cncf 1015-try-upstream
 # bash build.sh LFDLFoundation/landscape lfdl 33-switch-to-upstream
+git config --global user.email "info@cncf.io"
+git config --global user.name "Netlify Publisher"
 git commit -m 'lets test! [skip ci]' --allow-empty
-git push origin HEAD
+git push origin HEAD --tags
