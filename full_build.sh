8464154a6eb84873beced9e4db710a13cff0325a
ZeusTheTrueGod <l-2732@yandex.ru>
2018-12-20 18:04:03 +0000

    trigger builds of children projects

--- full_build.sh
+++ full_build.sh
@@ -2,6 +2,7 @@ set -e
 rm -rf dist || true
 mkdir -p dist
 
+# do not forget a build hook in the end of the file
 bash build.sh LFDLFoundation/lfdl-landscape lfdl
 bash build.sh cncf/landscape cncf
 
@@ -21,4 +22,6 @@ if [ $BRANCH = "master" ]; then
   git push github HEAD:master --tags --force
   echo "//registry.npmjs.org/:_authToken=$NPM_TOKEN" > ~/.npmrc
   yarn publish
+  curl -X POST -d {} https://api.netlify.com/build_hooks/5c1bd8e14ed62f166e8d9f7f
+  curl -X POST -d {} https://api.netlify.com/build_hooks/5c1bd968fdd72a78a54bdcd1
 fi
