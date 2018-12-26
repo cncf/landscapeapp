c4bbcd5217c06111227ef18f4e120e119350bf63
ZeusTheTrueGod <l-2732@yandex.ru>
2018-12-26 07:23:31 +0000

    final config changes

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -5,7 +5,7 @@ import {projectPath, settings} from './tools/settings';
 
 export default {
   resolve: {
-    modules: [path.resolve(projectPath, 'node_modules'), 'node_modules'],
+    // modules: [path.resolve(projectPath, 'node_modules'), 'node_modules'],
     extensions: ['*', '.js', '.jsx', '.json'],
     alias: {
       'project': path.resolve(projectPath),
