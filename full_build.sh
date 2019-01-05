0ec7c6230d001b0cf6100446d9dc0c3dba239231
ZeusTheTrueGod <l-2732@yandex.ru>
2019-01-05 16:41:16 +0000

    almost done

--- full_build.sh
+++ full_build.sh
@@ -3,11 +3,10 @@ rm -rf dist || true
 mkdir -p dist
 
 echo '<head><meta name="google-site-verification" content="27ZKkPQS2PWkd_0jqsSq4yUgUZ_BBTYjABudtQpMhXI" /></head>' > dist/index.html
-# do not forget a build hook in the end of the file
-# bash build.sh LFDLFoundation/lfdl-landscape lfdl
+bash build.sh LFDLFoundation/lfdl-landscape lfdl
 bash build.sh cncf/landscape cncf
 echo "User-agent: *" > dist/robots.txt
-
+# echo "Disallow: /" >> dist/robots.txt
 
 # This will increase a version and publish to an npm
 # If there is an existing package
