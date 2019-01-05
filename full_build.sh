14d16bbe8d96a1d1b690d69be4c3ce042ef9f73d
ZeusTheTrueGod <l-2732@yandex.ru>
2019-01-05 17:10:30 +0000

    prepare to merge

--- full_build.sh
+++ full_build.sh
@@ -2,11 +2,13 @@ set -e
 rm -rf dist || true
 mkdir -p dist
 
-echo '<head><meta name="google-site-verification" content="27ZKkPQS2PWkd_0jqsSq4yUgUZ_BBTYjABudtQpMhXI" /></head>' > dist/index.html
+# uncomment below when about to test a googlebot rendering
+# echo '<head><meta name="google-site-verification" content="27ZKkPQS2PWkd_0jqsSq4yUgUZ_BBTYjABudtQpMhXI" /></head>' > dist/index.html
 bash build.sh LFDLFoundation/lfdl-landscape lfdl
 bash build.sh cncf/landscape cncf
 echo "User-agent: *" > dist/robots.txt
-# echo "Disallow: /" >> dist/robots.txt
+# comment below when about to test a googlebot rendering
+echo "Disallow: /" >> dist/robots.txt
 
 # This will increase a version and publish to an npm
 # If there is an existing package
