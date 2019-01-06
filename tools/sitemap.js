8df911ef8db315923773677dbffcfc2c6e184ec9
Andrey Kozlov <l-2732@yandex.ru>
2019-01-06 22:30:01 +0000

    update a sitemap, close #3 (#81)
    * update a sitemap
    
    * remove non required license
    
    * add pdfs to the sitemap

--- tools/sitemap.js
+++ tools/sitemap.js
@@ -8,13 +8,37 @@ const sitemap = createSitemap({
   hostname: settings.global.website,
   cacheTime: 600 * 1000,
   urls: _.flatten([
-    { url: '/' },
+    _.values(settings.big_picture).map(function(section) {
+      return {
+        url: `images/${section.url}.pdf`,
+        img: [{
+          title: section.title,
+          url: `images/${section.url}.pdf`,
+          license: 'https://creativecommons.org/licenses/by/4.0/'
+        }]
+      }
+    }),
+    { url: '/',
+      img: _.values(settings.big_picture).map(function(section) {
+        return {
+          title: section.title,
+          url: `images/${section.url}.png`,
+          license: 'https://creativecommons.org/licenses/by/4.0/'
+        }
+      }).concat([{
+        title: `${settings.global.short_name} Landscape Logo`,
+        url: 'images/left-logo.svg',
+      }, {
+        title: `${settings.global.short_name} Logo`,
+        url: 'images/right-logo.svg',
+      }])
+    },
     items.map(function(item) {
       return {
         url: `selected=${item.id}`,
         img: [{
           url: item.href,
-          caption: `${item.name} logo`
+          title: `${item.name} logo`
         }]
       };
     })
