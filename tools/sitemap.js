f6377d2e4b6a01f4d958ee0d2877978cb123e6d8
ZeusTheTrueGod <l-2732@yandex.ru>
2019-01-06 21:52:33 +0000

    update a sitemap

--- tools/sitemap.js
+++ tools/sitemap.js
@@ -8,13 +8,29 @@ const sitemap = createSitemap({
   hostname: settings.global.website,
   cacheTime: 600 * 1000,
   urls: _.flatten([
-    { url: '/' },
+    { url: '/',
+      img: _.values(settings.big_picture).map(function(section) {
+        return {
+          title: section.title,
+          url: `images/${section.url}.png`,
+          license: 'https://creativecommons.org/licenses/by/4.0/'
+        }
+      }).concat([{
+        title: `${settings.global.short_name} Landscape Logo`,
+        url: `images/left-logo.svg`,
+        license: 'https://creativecommons.org/licenses/by/4.0/'
+      }, {
+        title: `${settings.global.short_name} Logo`,
+        url: `images/right-logo.svg`,
+        license: 'https://creativecommons.org/licenses/by/4.0/'
+      }])
+    },
     items.map(function(item) {
       return {
         url: `selected=${item.id}`,
         img: [{
           url: item.href,
-          caption: `${item.name} logo`
+          title: `${item.name} logo`
         }]
       };
     })
