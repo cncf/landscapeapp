c4bdd734374414d46a79a05cc9a5d5a9ab624411
ZeusTheTrueGod <l-2732@yandex.ru>
2019-01-28 15:14:57 +0000

    allow to specify tabs order from settings.yml

--- src/components/BigPicture/SwitchButtonContainer.js
+++ src/components/BigPicture/SwitchButtonContainer.js
@@ -5,14 +5,15 @@ import { filtersToUrl } from '../../utils/syncToUrl';
 import settings from 'project/settings.yml';
 import _ from 'lodash';
 
-const mainCard = [{ title: 'Card Mode', mode: 'card'}];
+const mainCard = [{ title: 'Card Mode', mode: 'card', tabIndex: 0}];
 const landscapes = _.map(settings.big_picture, function(section) {
   return {
     title: section.name,
-    mode: section.url
+    mode: section.url,
+    tabIndex: section.tab_index
   }
 });
-const cards = mainCard.concat(landscapes);
+const cards = _.orderBy(mainCard.concat(landscapes), 'tabIndex').map( item => _.pick(item, ['title', 'mode']));
 
 const mapStateToProps = (state) => ({
   mainContentMode: state.main.mainContentMode,
