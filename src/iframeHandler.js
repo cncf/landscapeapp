eff49b6834135c323cf0ad6e434d78d483c360e4
ZeusTheTrueGod <l-2732@yandex.ru>
2019-01-29 18:38:24 +0000

    moving part of an iframe management code to its own javascript file

---
+++ src/iframeHandler.js
@@ -0,0 +1,11 @@
+iFrameResize({
+  log:true,
+  messageCallback         : function(messageData){ // Callback fn when message is received
+    if (messageData.message.type === 'showModal') {
+      document.querySelector('body').style.overflow = 'hidden';
+    }
+    if (messageData.message.type === 'hideModal') {
+      document.querySelector('body').style.overflow = 'auto';
+    }
+  },
+}, '#landscape');
