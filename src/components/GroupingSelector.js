b123e948080bdfb9dbcdafd3e97d60bd89c27e7c
Andrey Kozlov <l-2732@yandex.ru>
2019-01-30 23:37:51 +0000

    use N/A for grouping in landscape mode, close #125 (#126)
    * use N/A for grouping in landscape mode, close #125
    
    * forgot a file, as usually

---
+++ src/components/GroupingSelector.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { pure } from 'recompose';
+import MenuItem from '@material-ui/core/MenuItem';
+import Select from '@material-ui/core/Select';
+import ComboboxSelector from './ComboboxSelector';
+
+
+const GroupingSelector = ({isBigPicture, value, options, onChange}) => {
+  if (!isBigPicture) {
+    return <ComboboxSelector value={value} options={options} onChange={onChange} />;
+  } else {
+    return (
+      <Select
+        disabled
+        value="empty"
+        style={{width:175 ,fontSize:'0.8em'}}
+      >
+        <MenuItem value="empty">
+          <em>N/A</em>
+        </MenuItem>
+      </Select>
+    );
+  }
+};
+export default pure(GroupingSelector);
