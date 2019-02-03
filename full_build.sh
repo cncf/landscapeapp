dd10b4f55bf44c116ecbb388a7de9fd16c52de33
ZeusTheTrueGod <l-2732@yandex.ru>
2019-02-03 22:57:24 +0000

    try an npm ci

--- full_build.sh
+++ full_build.sh
@@ -4,6 +4,7 @@ mkdir -p dist
 
 # uncomment below when about to test a googlebot rendering
 # echo '<head><meta name="google-site-verification" content="27ZKkPQS2PWkd_0jqsSq4yUgUZ_BBTYjABudtQpMhXI" /></head>' > dist/index.html
+npm ci
 bash build.sh LFDLFoundation/lfdl-landscape lfdl
 bash build.sh cncf/landscape cncf
 echo "User-agent: *" > dist/robots.txt
