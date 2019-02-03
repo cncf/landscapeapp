8e3ca2e7f5ba2698e754aa918a337ca2c63ea438
ZeusTheTrueGod <l-2732@yandex.ru>
2019-02-03 22:48:55 +0000

    test if this works

--- full_build.sh
+++ full_build.sh
@@ -25,6 +25,7 @@ if [ $BRANCH = "master" ]; then
   git push github HEAD:master
   git push github HEAD:master --tags --force
   echo "//registry.npmjs.org/:_authToken=$NPM_TOKEN" > ~/.npmrc
+  git checkout -- .
   npm publish || (sleep 5 && npm publish) || (sleep 30 && npm publish)
   curl -X POST -d {} https://api.netlify.com/build_hooks/5c1bd8e14ed62f166e8d9f7f
   curl -X POST -d {} https://api.netlify.com/build_hooks/5c1bd968fdd72a78a54bdcd1
