76a6074800556a049c5c7cd8f62a3953b8637160
ZeusTheTrueGod <l-2732@yandex.ru>
2019-02-06 13:39:58 +0000

    add a dom content loaded wrapper

--- src/iframeResizer.js
+++ src/iframeResizer.js
@@ -1,12 +1,13 @@
-
-iFrameResize({
-  log:true,
-  messageCallback         : function(messageData){ // Callback fn when message is received
-    if (messageData.message.type === 'showModal') {
-      document.querySelector('body').style.overflow = 'hidden';
-    }
-    if (messageData.message.type === 'hideModal') {
-      document.querySelector('body').style.overflow = 'auto';
-    }
-  },
-}, '#landscape');
+document.on('DOMContentLoaded', function() {
+  iFrameResize({
+    log:true,
+    messageCallback         : function(messageData){ // Callback fn when message is received
+      if (messageData.message.type === 'showModal') {
+        document.querySelector('body').style.overflow = 'hidden';
+      }
+      if (messageData.message.type === 'hideModal') {
+        document.querySelector('body').style.overflow = 'auto';
+      }
+    },
+  }, '#landscape');
+});
