296c816b932d9a997827ca7a88618b7c740e7885
ZeusTheTrueGod <l-2732@yandex.ru>
2019-02-08 10:39:41 +0000

    ensure that we use a latest npm version, close #145

--- full_build.sh
+++ full_build.sh
@@ -4,6 +4,7 @@ mkdir -p dist
 
 # uncomment below when about to test a googlebot rendering
 # echo '<head><meta name="google-site-verification" content="27ZKkPQS2PWkd_0jqsSq4yUgUZ_BBTYjABudtQpMhXI" /></head>' > dist/index.html
+npm install -g npm
 npm ci
 bash build.sh LFDLFoundation/lfdl-landscape lfdl
 bash build.sh cncf/landscape cncf
