cb0f4a83b050feb8c4f9faf2ed7960c53851c99a
ZeusTheTrueGod <l-2732@yandex.ru>
2019-02-13 21:04:18 +0000

    puppeteer fixes

--- tools/checkLinks.js
+++ tools/checkLinks.js
@@ -50,7 +50,8 @@ export async function checkUrl(url) {
 
   async function checkViaPuppeteer() {
     const puppeteer = require('puppeteer');
-    const browser = await puppeteer.launch();
+    const browser = await puppeteer.launch({args: ['--no-sandbox', '--disable-setuid-sandbox']});
+
     const page = await browser.newPage();
     page.setDefaultNavigationTimeout(120 * 1000);
     let result = null;
@@ -72,7 +73,7 @@ export async function checkUrl(url) {
 
   async function quickCheckViaPuppeteer() {
     const puppeteer = require('puppeteer');
-    const browser = await puppeteer.launch();
+    const browser = await puppeteer.launch({args: ['--no-sandbox', '--disable-setuid-sandbox']});
     const page = await browser.newPage();
     await page.setRequestInterception(true);
     page.on('request', request => {
