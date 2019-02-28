ab9c68a9ed6ccb095bff13ae1ea4968e86700529
ZeusTheTrueGod <l-2732@yandex.ru>
2019-02-28 15:58:20 +0000

    test

---
+++ 1.js
@@ -0,0 +1,5 @@
+require('process').stdout.write(".");
+require('process').stdout.write(".");
+require('process').stdout.write(".");
+require('process').stdout.write(".");
+require('process').stdout.write(".");
