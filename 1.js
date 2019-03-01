36a47ae45ce9b638199e2a926c0033be2fd765f9
Dan Kohn <dan@dankohn.com>
2019-03-01 15:24:29 -0500

    Delete 1.js

--- 1.js
+++
@@ -1,5 +0,0 @@
-require('process').stdout.write(".");
-require('process').stdout.write(".");
-require('process').stdout.write(".");
-require('process').stdout.write(".");
-require('process').stdout.write(".");
