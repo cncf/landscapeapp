995fe4e39c98aa347395f3c99fdc2ded9a485c25
ZeusTheTrueGod <l-2732@yandex.ru>
2019-03-17 13:36:55 +0000

    more checks

--- tools/ph.js
+++ tools/ph.js
@@ -1,44 +1,41 @@
+import Promise from 'bluebird'
 const phantom = require('phantom');
 
 (async function() {
   let status = null;
-  const instance = await phantom.create();
-  const pageUrl = 'https://www.cloudflare.com/asdffdsa';
+  const no = () => null;
+  const instance = await phantom.create(['--ignore-ssl-errors=yes', '--load-images=no'], {logger: {info: no, warning: no, error: no, debug: no}});
+  // const pageUrl = 'https://www.cloudflare.com';
+  // const pageUrl = 'https://www.tingyun.com/tingyun_app.html';
+  const pageUrl = 'https://www.habitat.sh/';
   const page = await instance.createPage();
   // await page.on('onResourceRequested', function(requestData) {
     // console.info('Requesting', requestData.url);
   // });
-  await page.on('onNavigationRequested', function(url, type, willNavigate, main) {
-    if (willNavigate && main) {
-      console.log('Trying to navigate to: ' + url);
-      console.log('Caused by: ' + type);
-      console.log('Will actually navigate: ' + willNavigate);
-      console.log('Sent from the page\'s main frame: ' + main);
-    }
-  });
   await page.on('onResourceReceived', function(response) {
     if (response.stage === 'end') {
       if (response.url === pageUrl + '/') {
-        console.info('a / redirect, it is ok');
         // nothings special
       }
       if (response.url === pageUrl || response.url === pageUrl + '/') {
-        if (response.code >= 400) {
-
-        }
-
-      }
-      console.info(response.contentType, response.url, response.status);
-      if (response.contentType === 'text/html') {
-        console.info(response);
+          status = response.status;
       }
     }
   });
 
   await page.open(pageUrl);
-  console.info(status);
-  //
-  const content = await page.property('content');
-  console.info('done');
+  await Promise.delay(5 * 1000);
+  const newUrl = await page.evaluate(function() {
+    return document.location.href
+  });
+  const withoutTrailingSlash = (x) => x.replace(/#(.*)/, '').replace(/\/$/, '');
+  console.info('Status: ', status, 'New url: ', newUrl);
+  if (withoutTrailingSlash(newUrl) !== withoutTrailingSlash(pageUrl)) {
+    console.info('redirect', withoutTrailingSlash(newUrl));
+  } else if (status >= 400) {
+    console.info('not found');
+  } else if (status === null) {
+    console.info('navigation error or timeout');
+  }
   await instance.exit();
 })();
