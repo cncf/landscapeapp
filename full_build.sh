a5a76fdf587310a114c86fccd3da44d540d073ec
ZeusTheTrueGod <l-2732@yandex.ru>
2019-04-11 21:10:19 +0100

    skip lf-edge dashboard for now

--- full_build.sh
+++ full_build.sh
@@ -8,7 +8,7 @@ npm install -g npm
 npm ci
 bash build.sh LFDLFoundation/lfdl-landscape lfdl
 bash build.sh cncf/landscape cncf
-bash build.sh lf-edge/lfedge-landscape lf-edge
+# bash build.sh lf-edge/lfedge-landscape lf-edge
 echo "User-agent: *" > dist/robots.txt
 # comment below when about to test a googlebot rendering
 echo "Disallow: /" >> dist/robots.txt
@@ -35,5 +35,5 @@ if [ $BRANCH = "master" ]; then
   echo 'Npm package published'
   curl -X POST -d {} https://api.netlify.com/build_hooks/5c1bd8e14ed62f166e8d9f7f
   curl -X POST -d {} https://api.netlify.com/build_hooks/5c1bd968fdd72a78a54bdcd1
-  curl -X POST -d {} https://api.netlify.com/build_hooks/5c80e31894c5c7758edb31e4
+  # curl -X POST -d {} https://api.netlify.com/build_hooks/5c80e31894c5c7758edb31e4
 fi
