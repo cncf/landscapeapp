6bc0461fee093c875bca294227a3229919bc6881
Jordi Noguera <jordinoguera83+github@gmail.com>
2019-08-19 15:27:06 +0200

    #283 Support googlebot=yes on query string

--- src/utils/isGoogle.js
+++ src/utils/isGoogle.js
@@ -1,4 +1,5 @@
 
 // GoogleBot will have a user agent ending with bot.html, look at https://support.google.com/webmasters/answer/1061943?hl=en for more details
-const isGoogle = navigator.userAgent.indexOf('bot.html') !== -1;
-export default isGoogle;
+const googlebotUA = navigator.userAgent.indexOf('bot.html') !== -1;
+const googlebotInQuery = window.location.search.indexOf('googlebot=yes') !== -1;
+export default googlebotUA || googlebotInQuery;
