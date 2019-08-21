1f0ca842d44d315fdc686a18eeb8358020820714
Jordi Noguera <jordinl83@users.noreply.github.com>
2019-08-21 17:19:26 +0200

    Fix URL shortening when filtering; fixes #218 (#300)
    * Backfill tests
    
    * Revert "Revert "#218 Fix URL filter params grouping (#295)""
    
    This reverts commit 36f0a39150417352f8f7a28c2aed196e0d258a20.
    
    * Make sure filtering by organization does not break
    
    * Move grouping logic to relation field

--- src/types/fields.js
+++ src/types/fields.js
@@ -25,7 +25,7 @@ const relationField = (function() {
   if (!firstEntry.children) {
     throw new Error('First entry of relation settings should have children!');
   }
-  const values = [{
+  const options = [{
     id: firstEntry.id,
     label: firstEntry.label,
     tag: firstEntry.tag,
@@ -48,7 +48,12 @@ const relationField = (function() {
     }
   }));
 
-  return { ...rootEntry, values: values};
+  return {
+    ...rootEntry,
+    values: options,
+    processValuesBeforeSaving: (values) => processValuesBeforeSaving({options, values}),
+    processValuesBeforeLoading: (values) => processValuesBeforeLoading({options, values})
+  };
 
 })();
 
