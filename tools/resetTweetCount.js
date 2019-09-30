5b58de4d7ebd93107bc19d3de5c7f5cba3990ebe
Jordi Noguera <jordinl83@users.noreply.github.com>
2019-09-30 17:24:31 +0200

    Manually assign Tweet count; Closes #369 (#373)

--- tools/resetTweetCount.js
+++ tools/resetTweetCount.js
@@ -1,6 +1,7 @@
 import updateProcessedLandscape from "./updateProcessedLandscape";
 
 updateProcessedLandscape((processedLandscape) => {
-  return { ...processedLandscape, twitter_options: { count: 0 } };
+  const count = process.argv[2] ? parseInt(process.argv[2]) : 0;
+  return { ...processedLandscape, twitter_options: { count } };
 });
 
