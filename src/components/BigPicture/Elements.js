fcb6d102faf28d7aaef3cede0ef986147eb650d0
Andrey Kozlov <l-2732@yandex.ru>
2019-10-02 20:48:05 +0100

    fix an issue with a zoom, close #350 (#377)

--- src/components/BigPicture/Elements.js
+++ src/components/BigPicture/Elements.js
@@ -240,7 +240,7 @@ const VerticalCategory = (function({header, subcategories, cols = 6, top, left, 
   const xRatio = 1.07;
   return (<div style={{}}>
     <div style={{
-      position: 'absolute', top: top -5 * zoom, left: left * zoom, height: height * zoom, margin: 5 * zoom, width: (width + 2) * zoom, background: 'white', border: `${1 * zoom}px solid ${color}`,
+      position: 'absolute', top: (top - 5) * zoom, left: left * zoom, height: height * zoom, margin: 5 * zoom, width: (width + 2) * zoom, background: 'white', border: `${1 * zoom}px solid ${color}`,
       boxShadow: `0 ${4 * zoom}px ${8 * zoom}px 0 rgba(0, 0, 0, 0.2), 0 ${6 * zoom}px ${20 * zoom}px 0 rgba(0, 0, 0, 0.19)`
     }} className="big-picture-section">
     <div style={{ width: width * zoom, height: 20 * zoom, lineHeight: `${20 * zoom}px`, textAlign: 'center', color: 'white', background: color, fontSize: 12 * zoom}}>
