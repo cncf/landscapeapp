4726caa7a13e0c78054ddce13767bad47b545812
ZeusTheTrueGod <l-2732@yandex.ru>
2019-10-09 15:37:16 +0100

    run on a remote server

--- build.sh
+++ build.sh
@@ -9,13 +9,13 @@ git checkout origin/$3
 cd ..
 export PROJECT_PATH=$2
 PROJECT_NAME=$2 npm run build
-mkdir -p dist
-cp -r $2/dist dist/$2
-rm -rf ./$2
-echo "/$2/* /$2/index.html 200" >> dist/_redirects
-echo "<div><a href="$2/"><h1>$2</h1></a></div>" >> dist/index.html
-echo "User-agent: *" > dist/$2/robots.txt
-echo "Disallow: /" >> dist/$2/robots.txt
+# mkdir -p dist
+# cp -r $2/dist dist/$2
+# rm -rf ./$2
+# echo "/$2/* /$2/index.html 200" >> dist/_redirects
+# echo "<div><a href="$2/"><h1>$2</h1></a></div>" >> dist/index.html
+# echo "User-agent: *" > dist/$2/robots.txt
+# echo "Disallow: /" >> dist/$2/robots.txt
 
 
 
--- tools/netlifyBuild.js
+++ tools/netlifyBuild.js
@@ -1,3 +1,5 @@
+import _ from 'lodash';
+import Promise from 'bluebird';
 const landscapesInfo = require('js-yaml').safeLoad(require('fs').readFileSync('landscapes.yml'));
 
 async function main() {
@@ -21,39 +23,87 @@ async function main() {
     console.info(maskSecrets(`We have a secret: ${secret}`));
   }
 
-  for (var landscape of landscapesInfo.landscapes) {
-    if (landscape.netlify !== 'full') {
-      console.info(`Skipping a build for ${landscape.name} because it has netlify: ${landscape.netlify}`);
-      continue;
-    }
-    console.info(`processing ${landscape.name} at ${landscape.repo}`);
+  // now our goal is to run this on a remote server. Step 1 - xcopy the repo
+  const folder = new Date().getTime();
+  const remote = 'root@147.75.76.177';
+  const result = require('child_process').spawnSync('bash', ['-lc', `
+      rsync --exclude="node_modules" -az -e ssh . ${remote}:/root/${folder}
+  `], {stdio: 'inherit'});
+  if (result.status !== 0) {
+    console.info(`Failed to rsync, exiting`);
+    process.exit(1);
+  }
+  console.info('Rsync done');
 
+  const results = await Promise.map(landscapesInfo.landscapes, async function(landscape) {
+
+    const vars = ['NODE_VERSION', 'RUBY_VERSION', 'CRUNCHBASE_KEY', 'GITHUB_KEY', 'TWITTER_KEYS'];
+
+    const outputFolder = landscape.name + new Date().getTime();
+    const dockerCommand = `
+      mkdir -p /root/${outputFolder}
+      chmod -R 777 /root/${outputFolder}
+      BASE_PATH=/root/build-image
+      REPO_PATH=/root/${folder}
+      OUTPUT_PATH=/root/${outputFolder}
+
+      ${vars.map( (v) => `${v}=${process.env[v]}` ).join('\n')}
+
+      docker run --rm -t \
+        ${vars.map( (v) => ` -e ${v} `).join(' ')} \
+        -v \${REPO_PATH}:/opt/repo \
+        -v \${OUTPUT_PATH}:/dist \
+        -v \${BASE_PATH}/run-build.sh:/usr/local/bin/build \
+        -v \${BASE_PATH}/run-build-functions.sh:/usr/local/bin/run-build-functions.sh \
+        buildbot /bin/bash -lc "build 'bash build.sh ${landscape.repo} ${landscape.name} master' && cp -r /opt/buildhome/repo/dist /dist"
+    `;
+
+    const bashCommand = `
+      nocheck=" -o StrictHostKeyChecking=no "
+      ssh $nocheck ${remote} << 'EOSSH'
+      ${dockerCommand}
+EOSSH
+      rsync -az -e ssh ${remote}:/root/${outputFolder}/dist/ dist/${landscape.name}
+    `
+
+    console.info(`processing ${landscape.name} at ${landscape.repo}`);
+    console.info(bashCommand);
+
+
+    // run a build command remotely for a given repo
 
     function runIt() {
       return new Promise(function(resolve) {
         var spawn = require('child_process').spawn;
-        var child = spawn('bash', ['build.sh', landscape.repo, landscape.name, 'master']);
+        var child = spawn('bash', ['-lc', bashCommand]);
+        let output = [];
         child.stdout.on('data', function(data) {
           const text = maskSecrets(data.toString('utf-8'));
-          process.stdout.write(text);
+          // console.info(text);
+          output.push(text);
           //Here is where the output goes
         });
         child.stderr.on('data', function(data) {
           const text = maskSecrets(data.toString('utf-8'));
-          process.stdout.write(text);
+          // console.info(text);
+          output.push(text);
           //Here is where the error output goes
         });
         child.on('close', function(returnCode) {
-          resolve({returnCode});
+          resolve({landscape, text: output.join(''), returnCode});
           //Here you can get the exit code of the script
         });
       });
     }
 
-    const {returnCode} = await runIt();
-    console.info(`${landscape.name} returned with a ${returnCode}`);
+    const output  = await runIt();
+    return output;
+  });
+  _.each(results, function(landscape) {
+    console.info(`Output from: ${landscape.landscape.name}, exit code: ${landscape.returnCode}`);
+    console.info(landscape.text);
+  });
 
-  }
 }
 main();
 
