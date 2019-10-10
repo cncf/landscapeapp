72eb59168bb254a68ada71932ac950a70617b405
ZeusTheTrueGod <l-2732@yandex.ru>
2019-10-10 10:13:06 +0100

    show output when building a single landscape

--- tools/parallel.js
+++ tools/parallel.js
@@ -1,7 +1,7 @@
 import Promise from 'bluebird';
 import _ from 'lodash';
 
-function runIt(task) {
+function runIt({task, showOutput}) {
   return new Promise(function(resolve) {
     let resolved = false;
     var spawn = require('child_process').spawn;
@@ -10,11 +10,17 @@ function runIt(task) {
     child.stdout.on('data', function(data) {
       const text = (data.toString('utf-8'));
       output.push(text);
+      if (showOutput) {
+        process.stdout.write(text);
+      }
       //Here is where the output goes
     });
     child.stderr.on('data', function(data) {
       const text = (data.toString('utf-8'));
       output.push(text);
+      if (showOutput) {
+        process.stdout.write(text);
+      }
       //Here is where the error output goes
     });
     child.on('close', function(returnCode) {
@@ -36,11 +42,19 @@ function runIt(task) {
 
 async function main() {
   const tasks = process.argv.slice(2);
-  const result = await Promise.map(tasks, runIt, {
-    concurrency: process.env.PARALLEL ? 100 : 1
+  const concurrency = process.env.PARALLEL ? 100 : 1;
+  if (concurrency !== 1) {
+    console.info(`Running ${tasks} in parallel`);
+  }
+  const result = await Promise.map(tasks, async function(task) {
+    return await runIt({task, showOutput: concurrency === 1});
+  }, {
+    concurrency: concurrency
   });
-  for (var r of result) {
-    console.info(`Task: ${r.task}`, r.text);
+  if (concurrency !== 1) {
+    for (var r of result) {
+      console.info(`Task: ${r.task}`, r.text);
+    }
   }
   const hasErrors = !! _.find(result, (r) => r.returnCode !== 0);
   process.exit(hasErrors ? 1 : 0);
