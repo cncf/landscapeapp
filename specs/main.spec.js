33d8e89949d698b97426c39eb25ddf1cd8bd83d9
Jordi Noguera <jordinoguera83+github@gmail.com>
2019-11-08 10:07:46 +0100

    Refactor test to loop over landscapes in settings.yml

--- specs/main.spec.js
+++ specs/main.spec.js
@@ -107,15 +107,15 @@ function landscapeTest() {
       await page.waitForSelector(".modal-content");
     }, 6 * 60 * 1000); //give it up to 1 min to execute
   });
-  if (settings.big_picture.extra) {
-    test("I visit an extra landscape page and have all required elements", async () => {
-      console.info('about to open an extra landscape page');
-      const page = await makePage(appUrl + '/format=' + settings.big_picture.extra.url);
+  Object.values(settings.big_picture).slice(1).forEach(({ name, url }) => {
+    test(`I visit ${name} landscape page and have all required elements`, async () => {
+      console.info(`about to open ${name} landscape page`);
+      const page = await makePage(`${appUrl}/format=${url}`);
       await page.waitForSelector('.big-picture-section');
       await page.click('.big-picture-section img[src]');
       await page.waitForSelector(".modal-content");
     }, 6 * 60 * 1000); //give it up to 1 min to execute
-  }
+  })
 }
 
 describe("Normal browser", function() {
