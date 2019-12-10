0ffd4ddce4559aa008ba87ad8a4237a9695afe89
Jordi Noguera <jordinoguera83+github@gmail.com>
2019-12-10 09:51:14 +0100

    Disable apple icons

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -88,6 +88,7 @@ export default {
         favicons: {
           appName: settings.global.name,
           icons: {
+            appleIcon: false,
             appleStartup: false,
             firefox: false,
             coast: false,
