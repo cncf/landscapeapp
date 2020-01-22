07e8ad20388dcea9b808d4208599af776c49afd1
Andrey Kozlov <l-2732@yandex.ru>
2020-01-22 14:13:41 +0000

    remove webpack noise, close #504 (#505)

--- tools/build.js
+++ tools/build.js
@@ -9,27 +9,27 @@ process.env.NODE_ENV = 'production'; // this assures React is built in prod mode
 
 console.log(chalkProcessing('Generating minified bundle. This will take a moment...'));
 
+
 webpack(config).run((error, stats) => {
   if (error) { // so a fatal error occurred. Stop here.
     console.log(chalkError(error));
-    return 1;
+    process.exit(1);
   }
 
   const jsonStats = stats.toJson();
 
-  if (jsonStats.hasErrors) {
-    return jsonStats.errors.map(error => console.log(chalkError(error)));
+  if (jsonStats.errors.length > 0) {
+    console.log(chalkWarning('Webpack generated the following errors: '));
+    jsonStats.errors.map(error => console.log(chalkError(error)));
+    process.exit(1);
   }
 
-  if (jsonStats.hasWarnings) {
+  if (jsonStats.warnings.length > 0) {
     console.log(chalkWarning('Webpack generated the following warnings: '));
     jsonStats.warnings.map(warning => console.log(chalkWarning(warning)));
   }
 
-  console.log(`Webpack stats: ${stats}`);
-
-  // if we got this far, the build succeeded.
   console.log(chalkSuccess('Your app is compiled in production mode in /dist. It\'s ready to roll!'));
 
-  return 0;
+  process.exit(0);
 });
