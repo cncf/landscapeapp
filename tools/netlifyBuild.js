42f7d11e69d3538a111efd748056a139f5c4edc4
Andrey Kozlov <l-2732@yandex.ru>
2020-03-14 01:12:11 +0000

    update redirects, close #534 (#535)
    * update redirects, close #534
    
    * a new version
    
    * duplicate the rule to _redirects

--- tools/netlifyBuild.js
+++ tools/netlifyBuild.js
@@ -121,8 +121,8 @@ EOSSH
     process.exit(1);
   }
   const redirects = results.map((result) => `
-    /${result.landscape.name}/ /${result.landscape.name}/prerender.html 200
-    /${result.landscape.name} /${result.landscape.name}/prerender.html 200
+    /${result.landscape.name}/ /${result.landscape.name}/prerender.html 200!
+    /${result.landscape.name} /${result.landscape.name}/prerender.html 200!
     /${result.landscape.name}/* /${result.landscape.name}/index.html 200
   `).join('\n');
   const index = results.map((result) => `<div><a href="${result.landscape.name}/"><h1>${result.landscape.name}</h1></a></div>`).join('\n');
