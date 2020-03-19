4135c41a92716ea01ccd0076d5f186a6fca404b8
Andrey Kozlov <l-2732@yandex.ru>
2020-03-19 23:05:20 +0000

    #75, weekly update (#538)
    * #75, weekly update
    
    * support a new version for a sitemap library
    
    * bug fix

--- tools/sitemap.js
+++ tools/sitemap.js
@@ -1,6 +1,6 @@
 import { getLandscapeCategories } from '../src/utils/sharedItemsCalculator';
 import fields from '../src/types/fields';
-import {createSitemap} from 'sitemap';
+import { SitemapStream } from 'sitemap';
 import { projectPath, settings } from './settings';
 import path from 'path';
 const items = JSON.parse(require('fs').readFileSync(path.resolve(projectPath, 'data.json')));
@@ -26,68 +26,83 @@ async function main() {
     }
   }));
 
-  const sitemap = createSitemap({
+
+  const stream = new SitemapStream({
     hostname: settings.global.website,
     cacheTime: 600 * 1000,
-    urls: _.flatten([
-      landscapeSettingsList.map(function(section) {
-        return {
-          url: `images/${section.url}.pdf`,
-          img: [{
-            title: section.title,
-            url: `images/${section.url}.pdf`,
-            license: 'https://creativecommons.org/licenses/by/4.0/'
-          }]
-        }
-      }),
-      _.orderBy(sectionsWithOrder, 'tab_index').map(function(orderEntry) {
-        if (orderEntry.key === 'card-mode') {
-          return {
-            url: '/format=card-mode',
-          };
-        }
-        const section = settings.big_picture[orderEntry.key];
-        return {
-          url: orderEntry.key === 'main' ? '/' : `/format=${section.url}`,
-          img: [{
-            title: section.title,
-            url: `images/${section.url}.png`,
-            license: 'https://creativecommons.org/licenses/by/4.0/'
-          }].concat ( orderEntry.key === 'main' ? [{
-            title: `${settings.global.short_name} Landscape Logo`,
-            url: 'images/left-logo.svg'
-          }, {
-            title: `${settings.global.short_name} Logo`,
-            url: 'images/right-logo.svg'
-          }] : [])
-        };
-      }),
-      items.map(function(item) {
-        const landscapeInfo = _.find(bigPictureElements, function(entry) {
-          return entry.categories.indexOf(item.category) !== -1;
-        });
-
-        const formatPart = (function() {
-          if (!landscapeInfo) {
-            return 'format=card-mode&'
-          }
-          if (!landscapeInfo.urlPart) {
-            return ''
-          }
-          return `format=${landscapeInfo.urlPart}&`;
-        })();
-
-        return {
-          url: `${formatPart}selected=${item.id}`,
-          img: [{
-            url: item.href,
-            title: `${item.name} logo`
-          }]
-        };
-      })
-    ])
   });
-  require('fs').writeFileSync(path.resolve(projectPath, 'dist/sitemap.xml'), sitemap);
+  const fileName = path.resolve(projectPath, 'dist/sitemap.xml');
+  const writeStream = require('fs').createWriteStream(fileName);
+  stream.pipe(writeStream);
+
+  const urls = _.flatten([
+    landscapeSettingsList.map(function(section) {
+      return {
+        url: `images/${section.url}.pdf`,
+        img: [{
+          title: section.title,
+          url: `images/${section.url}.pdf`,
+          license: 'https://creativecommons.org/licenses/by/4.0/'
+        }]
+      }
+    }),
+    _.orderBy(sectionsWithOrder, 'tab_index').map(function(orderEntry) {
+      if (orderEntry.key === 'card-mode') {
+        return {
+          url: '/format=card-mode',
+        };
+      }
+      const section = settings.big_picture[orderEntry.key];
+      return {
+        url: orderEntry.key === 'main' ? '/' : `/format=${section.url}`,
+        img: [{
+          title: section.title,
+          url: `images/${section.url}.png`,
+          license: 'https://creativecommons.org/licenses/by/4.0/'
+        }].concat ( orderEntry.key === 'main' ? [{
+          title: `${settings.global.short_name} Landscape Logo`,
+          url: 'images/left-logo.svg'
+        }, {
+          title: `${settings.global.short_name} Logo`,
+          url: 'images/right-logo.svg'
+        }] : [])
+      };
+    }),
+    items.map(function(item) {
+      const landscapeInfo = _.find(bigPictureElements, function(entry) {
+        return entry.categories.indexOf(item.category) !== -1;
+      });
+
+      const formatPart = (function() {
+        if (!landscapeInfo) {
+          return 'format=card-mode&'
+        }
+        if (!landscapeInfo.urlPart) {
+          return ''
+        }
+        return `format=${landscapeInfo.urlPart}&`;
+      })();
+
+      return {
+        url: `${formatPart}selected=${item.id}`,
+        img: [{
+          url: item.href,
+          title: `${item.name} logo`
+        }]
+      };
+    })
+  ]);
+
+  for (var url of urls) {
+    stream.write(url);
+  }
+  writeStream.on('error', (err) => {
+      console.error(Error(err))
+  })
+  stream.on('error', (err) => {
+      console.error(Error(err))
+  })
+  stream.end();
 }
 main().catch(function(ex) {
   console.info(ex);
