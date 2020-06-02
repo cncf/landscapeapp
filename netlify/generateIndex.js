f9685d807194c53768d1cf6e291f2670f0d0c34a
Jordi Noguera <jordinl83@users.noreply.github.com>
2020-06-02 15:58:40 +0200

    Make some landscapes required when building on Netlify (#554)

--- netlify/generateIndex.js
+++ netlify/generateIndex.js
@@ -9,19 +9,21 @@ module.exports = results => (`<!DOCTYPE html>
 <section class="section">
     <div class="container">
         <div class="columns is-multiline">
-            ${results.map(({ landscape }) => {
-  return `
+            ${results.map(({ landscape, exitCode }) => {
+                return `
                   <div class="column is-one-third-widescreen is-half-desktop is-full-tablet">
                     <div style="display: flex; justify-content: center">
                       <img src="${landscape.name}/images/right-logo.svg" style="height: 35px"
                            alt="${landscape.name}"/>
                     </div>
-                    <a href="${landscape.name}/" target="_blank" rel="noopener noreferrer">
-                      <img src="${landscape.name}/images/landscape_preview.png"/>
-                    </a>
+                    ${ exitCode === 0 ?
+                      `<a href="${landscape.name}/" target="_blank" rel="noopener noreferrer">
+                        <img src="${landscape.name}/images/landscape_preview.png"/>
+                       </a>` :
+                       `<div class="message is-danger is-large"><div class="message-body" style="border: none; text-align: center">FAILED</div></div>` }
                   </div>
                 `
-}).join('')}
+            }).join('')}
         </div>
     </div>
 </section>
