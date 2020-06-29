ec10e087f849b4ed904c9f486b713f12f6c24d99
Jordi Noguera <jordinoguera83+github@gmail.com>
2020-06-29 11:59:06 +0200

    Fix screenshots [Fix #636]

--- tools/renderLandscape.js
+++ tools/renderLandscape.js
@@ -1,6 +1,7 @@
 import Promise from 'bluebird';
 import { projectPath } from './settings';
 import { resolve } from 'path';
+import { writeFileSync } from 'fs';
 import { landscapeSettingsList } from "../src/utils/landscapeSettings";
 import { calculateSize, outerPadding, headerHeight } from "../src/utils/landscapeCalculations";
 
@@ -8,6 +9,14 @@ const getLastCommitSha = function() {
   return require('child_process').execSync(`cd '${projectPath}' && git log -n 1 --format=format:%h`).toString('utf-8').trim();
 }
 const port = process.env.PORT || '4000';
+
+const takeScreenshot = async ({ client, width, height, scale, path }) => {
+  const clip = { x: 0, y: 0, width, height, scale }
+  const result = await client.send('Page.captureScreenshot', { format: 'png', clip })
+  const buffer = Buffer.from(result.data, 'base64')
+  writeFileSync(path, buffer);
+}
+
 async function main() {
   const sha = await getLastCommitSha();
   const time = new Date().toISOString().slice(0, 19) + 'Z';
@@ -37,29 +46,30 @@ async function main() {
   let previews = [];
   previews = landscapeSettingsList.map(({ url }) => {
     const { width } = sizes[url]
-    const deviceScaleFactor = 960 / width;
+    const scale = 960 / width;
     const fileName = `${url}_preview.png`;
-    return { fileName, url, deviceScaleFactor };
+    return { fileName, url, scale };
   })
 
   const full_sizes = landscapeSettingsList.map(({ url }) => {
     const fileName = `${url}.png`;
     const pdfFileName = `${url}.pdf`;
-    return { fileName, pdfFileName, url, deviceScaleFactor: 4 };
+    return { fileName, pdfFileName, url, scale: 4 };
   });
 
   const browser = await puppeteer.launch({args: ['--no-sandbox', '--disable-setuid-sandbox', '--disable-gpu', '--disable-dev-shm-usage']});
   await Promise.mapSeries([previews, full_sizes], async function(series) {
     for (const pageInfo of series) {
-      const { url, deviceScaleFactor, fileName, pdfFileName } = pageInfo
+      const { url, scale, fileName, pdfFileName } = pageInfo
       const { width, height } = sizes[url]
       const page = await browser.newPage();
-      await page.setViewport({ width, height, deviceScaleFactor })
+      await page.setViewport({ width, height })
 
       const fullUrl = `http://localhost:${port}/${url}?version=${version}&scale=false&pdf`
       console.info(`visiting ${fullUrl}`);
       await page.goto(fullUrl, { waitUntil: 'networkidle0'});
-      await page.screenshot({ path: resolve(projectPath, 'dist', 'images', fileName), fullPage: false });
+      const path = resolve(projectPath, 'dist', 'images', fileName)
+      await takeScreenshot({ client: page._client, width, height, scale, path })
       if (pdfFileName) {
         await page.emulateMediaType('screen');
         await page.pdf({path: resolve(projectPath, 'dist', 'images', pdfFileName), width, height, printBackground: true, pageRanges: '1' });
