15a9bafe572feeb4707b3cfc4c35484e73eba15f
Andrey Kozlov <l-2732@yandex.ru>
2020-07-02 11:44:52 +0100

    remove a noise (#647)

--- tools/stopOldCiServer.js
+++ tools/stopOldCiServer.js
@@ -1,5 +1,3 @@
 const result = require('child_process').spawnSync('bash', ['-lc',
   " kill -9 `ps ux | grep ciServer | grep -v grep | awk -F ' ' '{print $2}'` "
 ]);
-console.info(result.stdout.toString('utf-8'));
-console.info(result.stderr.toString('utf-8'));
