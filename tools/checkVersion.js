e54124a5bcd336a1042e94d2ce71c9667a29eeb0
Andrey Kozlov <l-2732@yandex.ru>
2020-07-06 22:38:28 +0100

    more progress

--- tools/checkVersion.js
+++ tools/checkVersion.js
@@ -1,4 +1,5 @@
 import rp from 'request-promise';
+import chalk from 'chalk'
 
 const maxHours = 4;
 function reportProblem() {
@@ -8,23 +9,28 @@ export default async function check() {
     headers: {'User-Agent': 'curl'},
     json: true
   });
+  const currentBranch = require('child_process').execSync(`git rev-parse --abbrev-ref HEAD`).toString().trim();
+  if (currentBranch !== 'master') {
+    return;
+  }
   const remoteDate = result.commit.commit.committer.date;
   const localDate = require('child_process').execSync('git show -s --format=%ci master').toString().trim()
 
-  console.info(new Date(localDate), new Date(remoteDate));
   if ( new Date(remoteDate).getTime() !== new Date(localDate).getTime()) {
+    if (process.env.SKIP_VERSION_CHECK) {
+      console.info(`Skipping version check`);
+      return;
+    }
     console.error(`
-!!!Fatal Error!!!
+${chalk.red('!!!Fatal Error!!!')}
 Your copy of cncf/landscapeapp repo is not up to date!
-The latest commit to a master branch occurred at ${remoteDate.toISOString()}
-Your latest commit is dated ${localDate.toISOString()}
-Trying to pull a latest version for you
-    `);
-    const gitPull = require('child_process').execSync('git pull --ff-only').toString().trim();
-    console.error(gitPull);
-    console.error(`
-Tried to update to the latest version. Now, please start a script again
-    `);
+The latest commit to a master branch occurred at ${new Date(remoteDate).toISOString()}
+Your latest commit is dated ${new Date(localDate).toISOString()}
+If you are sure that is ok, run this command: "export SKIP_VERSION_CHECK=1" and rerun the script again
+You can use the command below to update your landscapeapp repo to the latest master branch:
+    ${chalk.green(`(cd ${process.cwd()} && git pull)`)}
+After updating the landscapeapp repo, rerun the script again
+`);
   }
 }
 check();
