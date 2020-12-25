d7ba8b09b75e9b84abefe16b763eb678e8c56c12
Jordi Noguera <jordinoguera83+github@gmail.com>
2020-12-25 11:05:57 +0100

    Set title for modal page

--- src/pages/[[...path]].js
+++ src/pages/[[...path]].js
@@ -1,4 +1,3 @@
-import createSelector from '../utils/createSelector';
 import HomePageComponent from '../components/HomePage';
 import {showFilters, hideFilters, closeDialog } from '../reducers/mainReducer';
 import isEmbed from '../utils/isEmbed';
@@ -17,19 +16,6 @@ import { useContext } from 'react'
 import RootContext from '../contexts/RootContext'
 
 const defaultTitle =  settings.global.meta.title;
-
-// TODO: sort this
-const getTitle = createSelector([state => state], function(state) {
-    if (!state.main.ready) {
-      return defaultTitle;
-    }
-    const groupedItems = getGroupedItems(state);
-    const groupedItemsForBigPicture = getGroupedItemsForBigPicture(state);
-    const selectedItemId = state.main.selectedItemId;
-    const isBigPicture = state.main.mainContentMode !== 'card';
-    const selectedItemInfo = selectedItemCalculator(groupedItems, groupedItemsForBigPicture, selectedItemId, isBigPicture);
-    return selectedItemInfo.hasSelectedItem ? `${selectedItemInfo.itemInfo.name} - ${defaultTitle}` : defaultTitle;
-});
 
 const mapStateToProps = (state) => ({
   ready: state.main.ready,
@@ -62,9 +48,11 @@ const HomePage = ({ entries, selectedItem }) => {
     router.push(url)
   }
 
+  const title = selectedItem ? `${selectedItem.name} - ${defaultTitle}` : defaultTitle
+
   return <EntriesContext.Provider value={{entries, selectedItem, navigate, groupedItems, nextItemId, previousItemId }}>
     <Head>
-      <title>{defaultTitle}</title>
+      <title>{title}</title>
     </Head>
     <HomePageComponent />
   </EntriesContext.Provider>
