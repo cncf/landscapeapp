0367a2997d3556ff8a0b6ea7df084b2924251402
Jordi Noguera <jordinoguera83+github@gmail.com>
2020-12-27 20:49:36 +0100

    Enable relation filter

--- src/components/ProjectFilterContainer.js
+++ src/components/ProjectFilterContainer.js
@@ -1,17 +1,15 @@
 import TreeSelector from './TreeSelector';
-import { changeFilter } from '../reducers/mainReducer';
 import { options } from '../types/fields';
 import { useContext } from 'react'
 import RootContext from '../contexts/RootContext'
-
-const onChange = function(newValue) {
-  return changeFilter('relation', newValue);
-}
+import EntriesContext from '../contexts/EntriesContext'
 
 const ProjectFilterContainer = () => {
   const { params } = useContext(RootContext)
+  const { navigate } = useContext(EntriesContext)
   const value = params.filters.relation
   const _options = options('relation')
+  const onChange = relation => navigate({ filters: { relation }})
   return <TreeSelector onChange={onChange} options={_options} value={value} />
 }
 
