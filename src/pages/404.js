56aa2dcd6b19c9e280b859b325280958a7310bf7
Jordi Noguera <jordinoguera83+github@gmail.com>
2021-01-12 09:50:45 +0100

    Redirect legacy URLs to new format

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,13 +1,29 @@
 import { useEffect } from 'react'
 import { useRouter } from 'next/router'
+import { parse, stringify } from 'querystring'
+
+const checkUrl = () => {
+  const pathname = location.pathname.replace(/^\//, '')
+  if (pathname.indexOf('=') >= 0) {
+    const notice = `URL deprecated: ${window.location.href}`
+    const { format, ...rest } = parse(pathname)
+    const path = `/${format}`
+    const query = stringify(rest)
+    const redirectUrl = [path, query].filter(_ => _).join('?')
+    return { redirectUrl, notice }
+  } else {
+    const notice = `URL not found: ${window.location.href}`
+    return { redirectUrl: '/', notice }
+  }
+}
 
 const NotFoundPage = ({ setError }) => {
   const router = useRouter()
 
   useEffect(() => {
-    const message = `URL not found: ${window.location.href}`
-    router.push('/')
-    return () => setError(message)
+    const { redirectUrl, notice } = checkUrl()
+    router.push(redirectUrl)
+    return () => setError(notice)
   }, [])
 
   return <div>Redirecting...</div>
