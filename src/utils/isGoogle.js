c3526fe9a164c0af3351d43358a9df722aa0ceeb
Jordi Noguera <jordinoguera83+github@gmail.com>
2021-01-13 15:11:09 +0100

    Add TODO items

--- src/utils/isGoogle.js
+++ src/utils/isGoogle.js
@@ -1,6 +1,7 @@
 // GoogleBot will have a user agent ending with bot.html, look at https://support.google.com/webmasters/answer/1061943?hl=en for more details
 import isBrowser from './isBrowser'
 
+// TODO: rework
 const isGoogle = () => {
   if (!isBrowser()) {
     return false
