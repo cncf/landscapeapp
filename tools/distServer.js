c30410cb9436d7502fab2d02895e58f72d16e570
Jordi Noguera <jordinoguera83+github@gmail.com>
2021-01-14 16:39:03 +0100

    Add compression to dist server

--- tools/distServer.js
+++ tools/distServer.js
@@ -4,12 +4,14 @@ import express from 'express'
 import serveStatic from 'serve-static'
 import {chalkProcessing} from './chalkConfig'
 import { projectPath } from './settings'
+import compression from 'compression'
 
 const app = express()
 const distPath = path.resolve(projectPath, 'dist')
 
 console.log(chalkProcessing('running a dist server on http://localhost:4000 ...'));
 
+app.use(compression({ level: 6 }))
 app.use(serveStatic(distPath, { redirect: false, cacheControl: false }));
 app.use((req, res, next) => {
   const urlPath = req.url.split('?')[0]
@@ -17,7 +19,7 @@ app.use((req, res, next) => {
   const indexPath = `${projectPath}/dist${urlPath}/index.html`
   if (existsSync(filePath)) {
     res.end(readFileSync(filePath, 'utf-8'))
-  } else  if(existsSync(indexPath)) {
+  } else if(existsSync(indexPath)) {
     res.end(readFileSync(indexPath, 'utf-8'))
   } else {
     next()
