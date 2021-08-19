176edbf0be2bd1c8d1e4fc27cc45d452aec8e3c1
Jordi Noguera <jordinoguera83+github@gmail.com>
2021-08-19 16:47:48 +0200

    Add title to guide

--- src/pages/guide.js
+++ src/pages/guide.js
@@ -126,6 +126,7 @@ const GuidePage = ({ content, title, entries, mainContentMode }) => {
           </div>
         </div>
         <div className="main">
+          <h1 className="title">{settings.global.name} guide</h1>
           {
             nodes.map(node => {
               const subcategoryEntries = node.subcategory && enhancedEntries.filter(entry => entry.path.split('/')[1].trim() === node.title)
