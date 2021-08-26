07c9a8a2b7c7a62f3165f54be90c9d553b827f21
Jordi Noguera <jordinl83@users.noreply.github.com>
2021-08-26 15:42:15 +0200

    Fix og:image (#766)

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -11,7 +11,6 @@ import { useEffect, useState } from 'react'
 import { useRouter } from 'next/router'
 import isBrowser from '../utils/isBrowser'
 import useCurrentDevice from '../utils/useCurrentDevice'
-import assetPath from '../utils/assetPath'
 import { parse } from 'query-string'
 
 const Notice = ({ onClose, notice }) => {
@@ -26,7 +25,7 @@ const Notice = ({ onClose, notice }) => {
 export default function App({ Component, pageProps }) {
   const router = useRouter()
   const description = `${settings.global.meta.description}. Updated: ${process.env.lastUpdated}`
-  const favicon = assetPath('images/favicon.png')
+  const favicon = `${settings.global.website}/images/favicon.png`
   const [notice, setNotice] = useState(null)
   const currentDevice = useCurrentDevice()
   // TODO: hydration fix
