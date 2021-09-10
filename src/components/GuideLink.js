4a87f2c0470604ec1716ca23c992be4feb77bc61
Jordi Noguera <jordinoguera83+github@gmail.com>
2021-09-10 17:13:09 +0200

    Fix guide link in deploy preview

--- src/components/GuideLink.js
+++ src/components/GuideLink.js
@@ -2,11 +2,12 @@ import React from 'react'
 import InfoIcon from '@material-ui/icons/InfoOutlined'
 import { getContrastRatio } from '@material-ui/core/styles'
 import OutboundLink from './OutboundLink'
+import assetPath from '../utils/assetPath'
 
 const GuideLink = ({ color, identifier, label, fontSize = 18 }) => {
   const style = { width: fontSize, height: fontSize, display: 'block' }
   const ariaLabel = `Read more about ${label} on the guide`
-  return <OutboundLink to={`/guide#${identifier}`} style={style} aria-label={ariaLabel}>
+  return <OutboundLink to={assetPath(`/guide#${identifier}`)} style={style} aria-label={ariaLabel}>
     <InfoIcon style={{
       color: !color || getContrastRatio('#ffffff', color) < 4.5 ? '#282828' : '#ffffff',
       fontSize
