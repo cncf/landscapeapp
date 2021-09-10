3e196c49db7cb8c7b5c9e5411c21239692482515
Jordi Noguera <jordinoguera83+github@gmail.com>
2021-09-10 16:02:16 +0200

    Move sidebar to left and add padding between sidebar and main content

--- src/pages/guide.js
+++ src/pages/guide.js
@@ -1,15 +1,14 @@
-import React, { useState } from 'react'
+import React, { useState, Fragment, useEffect } from 'react'
 import { existsSync, readFileSync } from 'fs'
 import Typography from '@material-ui/core/Typography'
 import classNames from 'classnames'
 import MenuIcon from '@material-ui/icons/Menu'
 import IconButton from '@material-ui/core/IconButton'
-import RoomIcon from '@material-ui/icons/Room'
 import CloseIcon from '@material-ui/icons/Close'
+import ArrowRightIcon from '@material-ui/icons/ArrowRight'
 import Link from 'next/link'
 import { useRouter } from 'next/router'
 import Head from 'next/head'
-import Header from '../components/Header'
 import assetPath from '../utils/assetPath'
 import { LandscapeProvider } from '../contexts/LandscapeContext'
 import { parseParams } from '../utils/routing'
@@ -79,19 +78,45 @@ const SubcategoryMetadata = ({ node, entries }) => {
   </>
 }
 
-const TreeNavigation = ({ nodes, currentSection }) => {
+const TreeNavigation = ({ nodes, hideSidebar }) => {
+  const router = useRouter()
+  const currentSection = router.asPath.split('#')[1]
+  const defaultExpanded = nodes.reduce((acc, node) => {
+    return { ...acc, [node.identifier]: currentSection && currentSection.indexOf(node.identifier) === 0 }
+  }, {})
+  const [expandedItems, setExpandedItems] = useState(defaultExpanded)
+  const [isReady, setIsReady] = useState(false)
+
+  useEffect(() => setIsReady(true), [])
+
   return nodes.map(node => {
-    const showChildren = currentSection && currentSection.indexOf(node.identifier) === 0 && Array.isArray(node.content)
+    const hasChildren = Array.isArray(node.content)
+    const children = hasChildren && [
+      { ...node, content: null, title: 'Overview', level: node.level + 1 },
+      ...node.content
+    ]
+    const isExpanded = isReady && expandedItems[node.identifier]
+    const showChildren = isExpanded && hasChildren
+    const className = `sidebar-link ${isReady && node.identifier === currentSection ? 'active' : ''}`
+    const expandableClass = `sidebar-link expandable ${isExpanded ? 'expanded' : ''}`
+    const paddingLeft = 20 + (node.level - 1) * 10
+    const toggleExpanded = e => {
+      e.preventDefault()
+      setExpandedItems({ ...expandedItems, [node.identifier]: !isExpanded })
+    }
 
-    return node.title && <div key={node.identifier} className="sidebar-level">
-      <>
-        <Link href={`#${node.identifier}`} prefetch={false}>
-          <a className="sidebar-link" style={{ color: node.identifier === currentSection && 'black' }}>{node.title}</a>
-        </Link>
+    return node.title && <Fragment key={node.identifier}>
+      { hasChildren ?
+          <a className={expandableClass} href='#' onClick={toggleExpanded} style={{ paddingLeft }}>
+            {node.title} <ArrowRightIcon />
+          </a> :
+          <Link href={`#${node.identifier}`} prefetch={false}>
+            <a className={className} style={{ paddingLeft }} onClick={hideSidebar}>{node.title}</a>
+          </Link>
+      }
 
-        { showChildren && <TreeNavigation nodes={node.content} currentSection={currentSection} /> }
-      </>
-    </div>
+      { showChildren && <TreeNavigation nodes={children} hideSidebar={hideSidebar} /> }
+    </Fragment>
   })
 }
 
@@ -99,7 +124,7 @@ const TreeContent = ({ nodes, enhancedEntries }) => {
   return nodes.map(node => {
     const subcategoryEntries = node.subcategory && enhancedEntries.filter(entry => entry.path.split('/')[1].trim() === node.title)
 
-    return <div key={node.identifier} id={node.title && node.identifier} >
+    return <div key={node.identifier} id={node.title && node.identifier} className="guide-section">
       { node.title && <Typography variant={`h${node.level + 1}`}>
         { node.permalink && <a href={assetPath(`/card-mode?category=${node.permalink}`)} target="_blank" className="permalink">
           <span className="guide-icon" />{node.title}
@@ -130,11 +155,13 @@ const GuidePage = ({ content, title, entries, mainContentMode }) => {
   })
 
   const [sidebarVisible, setSidebarVisible] = useState(false)
-  const showSidebar = _ => setSidebarVisible(true)
-  const hideSidebar = _ => setSidebarVisible(false)
-
-  const { asPath } = useRouter()
-  const currentSection = asPath.split('#')[1]
+  const toggleSidebar = visible => {
+    const { classList } = document.body
+    visible ? classList.add('no-scroll-mobile') : classList.remove('no-scroll-mobile')
+    setSidebarVisible(visible)
+  }
+  const showSidebar = _ => toggleSidebar(true)
+  const hideSidebar = _ => toggleSidebar(false)
 
   return <LandscapeProvider entries={entries} pageParams={{ mainContentMode }}>
     <Head>
@@ -143,7 +170,7 @@ const GuidePage = ({ content, title, entries, mainContentMode }) => {
 
     {selectedItemId && <ItemDialog/>}
 
-    <div id="guide-page" className={classNames('app',{'filters-opened' : sidebarVisible})}>
+    <div id="guide-page" className={classNames('app',{'sidebar-open' : sidebarVisible})}>
       <div className="side-content">
         <span className="landscape-logo">
           <InternalLink to="/">
@@ -151,28 +178,37 @@ const GuidePage = ({ content, title, entries, mainContentMode }) => {
           </InternalLink>
         </span>
 
-        <div className="sidebar-show">
-          <IconButton title="Show sidebar" onClick={showSidebar}><MenuIcon /></IconButton>
-        </div>
-
         <div className="guide-sidebar">
-          <IconButton className="sidebar-collapse" title="Hide sidebar" onClick={hideSidebar}><CloseIcon /></IconButton>
-          <TreeNavigation nodes={content} currentSection={currentSection} />
+          <IconButton className="sidebar-collapse" title="Hide sidebar" onClick={hideSidebar}>
+            <CloseIcon />
+          </IconButton>
+          <TreeNavigation nodes={content} hideSidebar={hideSidebar} />
         </div>
       </div>
 
-      <div className="main-content">
-        <Typography variant="h1" className="title">{settings.global.short_name} Landscape Guide</Typography>
+      <div className="guide-header">
+        <div className="container">
+          <div className="content">
+            <div className="sidebar-show">
+              <IconButton title="Show sidebar" onClick={showSidebar}><MenuIcon  /></IconButton>
+            </div>
+
+            <Typography variant="h1" className="title">{settings.global.short_name} Landscape Guide</Typography>
+          </div>
+        </div>
 
         <OutboundLink eventLabel={short_name} to={company_url} className="landscapeapp-logo" title={`${short_name} Home`}>
           <img src={assetPath("/images/right-logo.svg")} title={`${short_name} Logo`}/>
         </OutboundLink>
+      </div>
 
-        <div className="guide-content">
-          <TreeContent nodes={content} enhancedEntries={enhancedEntries} />
+      <div className="main-content">
+        <div className="container">
+          <div className="content">
+            <TreeContent nodes={content} enhancedEntries={enhancedEntries} />
+          </div>
         </div>
       </div>
-      <div className="app-overlay"></div>
     </div>
   </LandscapeProvider>
 }
