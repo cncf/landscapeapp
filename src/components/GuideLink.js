a31846bd7fb049237bc89c9319dcdd693399dc59
Jordi Noguera <jordinoguera83+github@gmail.com>
2021-09-23 17:37:58 +0200

    Make category title and info icon bolder on hover

--- src/components/GuideLink.js
+++ src/components/GuideLink.js
@@ -1,15 +1,14 @@
 import React from 'react'
 import InfoIcon from '@material-ui/icons/InfoOutlined'
-import { getContrastRatio } from '@material-ui/core/styles'
 import OutboundLink from './OutboundLink'
 import assetPath from '../utils/assetPath'
 
-const GuideLink = ({ identifier, label, className="", fontSize = 18 }) => {
+const GuideLink = ({ identifier, label, className="" }) => {
   const ariaLabel = `Read more about ${label} on the guide`
   const to = assetPath(`/guide#${identifier}`)
   return <OutboundLink className={`${className} guide-info-link`} to={to} aria-label={ariaLabel}>
     <InfoIcon style={{
-      fontSize
+      fontSize: 'inherit'
     }}/>
   </OutboundLink>
 }
