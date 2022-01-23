c842921b15b775af5bc02874c1e9eaaa78c999cd
homeant <homeanter@163.com>
2022-01-23 16:38:37 +0800

    fix env at start error

--- .babelrc.js
+++ .babelrc.js
@@ -1,5 +1,10 @@
 const path = require('path')
 
+
+const env = require('@next/env')
+const projectDir = process.cwd()
+env.loadEnvConfig(projectDir)
+
 // CAREFUL before adding more presets, next/babel already includes some
 // see https://nextjs.org/docs/advanced-features/customizing-babel-config
 module.exports = {
