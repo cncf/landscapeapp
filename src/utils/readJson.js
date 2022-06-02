274830460e63fcf78ae9da3079d6730c607a09ea
Andrey Kozlov <zeus@Andreys-MacBook-Pro.local>
2022-06-02 13:28:30 +0100

    fix

--- src/utils/readJson.js
+++ src/utils/readJson.js
@@ -1,10 +1,12 @@
 // allows to read json files from certain directores
 
 module.exports.readJsonFromProject = function(file) {
-  if (global.lookups && global.lookups[file]) {
-    return global.lookups[file];
-  } else {
-    throw ('Can not read ', file);
+  if (global.lookups) {
+    if (global.lookups[file]) {
+      return global.lookups[file];
+    } else {
+      throw ('Can not read ', file);
+    }
   }
   const fullPath = require('path').resolve(process.env.PROJECT_PATH, file + '.json');
   return JSON.parse(require('fs').readFileSync(fullPath, 'utf-8'));
