544ba79b1dc4e8dd83551160b1932e25356bdff2
Andrey Kozlov <zeus@Andreys-MacBook-Pro.local>
2022-08-09 16:26:56 +0100

    update the saneName

--- src/utils/saneName.js
+++ src/utils/saneName.js
@@ -1,5 +1,5 @@
 const  _ = require('lodash');
 const { paramCase } = require('change-case');
 module.exports.saneName = function(x) {
-  return _.deburr(paramCase(x));
+  return _.deburr(paramCase(x.replace(/\s/g,'spaceseparator')).replaceAll('spaceseparator', '-'));
 };
