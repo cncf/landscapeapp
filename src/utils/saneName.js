921e8a3cdf11f9ab6ca123e380850aa5b684dfb8
Andrey Kozlov <zeus@Andreys-MacBook-Pro.local>
2022-08-12 16:15:54 +0100

    saneName to affect only existing items

--- src/utils/saneName.js
+++ src/utils/saneName.js
@@ -1,5 +1,12 @@
 const  _ = require('lodash');
 const { paramCase } = require('change-case');
+let hash = {};
 module.exports.saneName = function(x) {
-  return _.deburr(paramCase(x.replace(/\s/g,'spaceseparator')).replaceAll('spaceseparator', '-'));
+  let result = _.deburr(paramCase(x));
+  if (hash[result] && hash[result] !== x) {
+    result = result + '-2';
+    console.info(`Hash for ${x} is ${result}`);
+  }
+  hash[result] = x;
+  return result;
 };
